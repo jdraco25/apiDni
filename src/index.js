6887a88b12a9f58e0b77a0eb788eaee17d66d475
jdraco25 <darkdraco_25@hotmail.com>
2021-12-15 19:44:13 -0500

    ddd

--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
-/*const app = require('./app');
+const app = require('./app');
 //require('./database');
 /*require('./DBUtil');
 require('./DBUtil_user');
 require('./DB_sqlServer');
-require('./controllers/verifyToken');
+require('./controllers/verifyToken');*/
 async function init() {
     await app.listen(3000);
     console.log('Server on port 3000');
 }
 console.log('inicializado');
 
-init();*/
-
-const express = require('express');
-const app = express();
-app.get('/', async(req, res) => {
-    console.log('ingreso')
-    res.send('Hello world with express!');
-});
-
-app.listen(3000)
-console.log('server on port 3000 prueba ')
+init();
